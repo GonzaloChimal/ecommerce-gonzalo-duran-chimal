739f920dd124e1660fdc13b29705e25028c5a5d9
Gonzalo Chimal <duran.chimal.gonzalo.1992@gmail.com>
2022-06-30 21:59:34 -0500

    Update client-service.js

--- services/client-service.js
+++ services/client-service.js
@@ -1,41 +1,49 @@
-//Conexion
-const listaProductos = () => fetch('https://app-products-alurageek.herokuapp.com/productos').then(respuesta => respuesta.json());
+// Resultados de busqueda de productos desde la barra de busqueda.
+const buscarProductos = (query) => fetch(`https://app-products-alurageek.herokuapp.com/productos?q=${query}`).then(res => res);
 
-const crearProducto = (nombre, precio, imagen, categoria, descripcion) => {
-  return fetch('https://app-products-alurageek.herokuapp.com/productos', {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({nombre, precio, imagen,  id:uuid.v4(), categoria, descripcion})
-  });
+// CRUD, Accediendo a la lista de productos de la tienda.
+const listaProductos = () => fetch(`https://app-products-alurageek.herokuapp.com/productos`).then(res => res);
+
+// CRUD, Creado un nuevo producto para la tienda.
+const crearProducto = (imagen, nombre, precio, descripcion, categoria) => {
+    return fetch(`https://app-products-alurageek.herokuapp.com/productos`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({imagen , nombre, precio, descripcion, id: uuid.v4(), categoria})
+    });
 };
 
+// CRUD, Eliminado un producto existente en la tienda.
 const eliminarProducto = (id) => {
-  return fetch(`https://app-products-alurageek.herokuapp.com/productos${id}`, {
-    method: "DELETE"
-  });
+    return fetch(`https://app-products-alurageek.herokuapp.com/productos/${id}`, {
+        method: "DELETE"
+    });
 };
 
-//Detalles del producto por ID
-const detalleProducto = async (id) => {
-  return fetch(`https://app-products-alurageek.herokuapp.com/productos${id}`).then( respuesta => respuesta.json());
-};
+// CRUD, Accediendo a todos los detalles de los productos de la tienda.
+const detalleProducto = (id) => {
+    return fetch(`https://app-products-alurageek.herokuapp.com/productos/${id}`).then(res => res)
+}
 
-const actualizarProducto = (nombre, precio, imagen, id, categoria, descripcion) => {
-  return fetch(`https://app-products-alurageek.herokuapp.com/productos${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({nombre, precio, imagen, categoria, descripcion})
-  }).then(respuesta => respuesta).catch(error => console.log(error));
-};
+// CRUD, Actualizando datos de un producto de la tienda.
+const actualizarProducto = (imagen, nombre, precio, id, descripcion, categoria) => {
+    return fetch(`https://app-products-alurageek.herokuapp.com/productos/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({imagen, nombre, precio, descripcion, categoria})
+    })
+}
 
-export const clientServices = {
-  listaProductos,
-  detalleProducto,
-  crearProducto,
-  actualizarProducto,
-  eliminarProducto
-};
+// Exportar las funciones para utilizarlas en las respectivas consultas.
+export const productServices = {
+    buscarProductos,
+    crearProducto,
+    eliminarProducto,
+    detalleProducto,
+    actualizarProducto,
+    listaProductos
+}
